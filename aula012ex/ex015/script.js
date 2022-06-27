ca41af4287a007cb8c938bdc37d3a21c364a8d76
Victor Guilherme dos Santos Correa <78057195+Victor6345@users.noreply.github.com>
2022-06-27 11:55:25 -0300

    atualização

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -9,11 +9,37 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.getElementById('imagem')
         if (fsex[0].checked ){
             genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+               img.src = 'imagens/bebe-menino.jpg'
+            }
+            else if (idade < 21){
+               img.src = 'imagens/jovem-homem.jpg'
+            }
+            else if (idade < 50){
+               img.src = 'imagens/homem-adulto.jpg'
+            }
+            else{
+               img.src = 'imagens/idoso.jpg'
+            }
        } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 20){
+               img.src = 'imagens/bebe-menina.jpg'
+            }
+            else if (idade < 21){
+               img.src = 'imagem/jovem-mulher.jpg'
+            }
+            else if (idade < 50){
+               img.src = 'imagem/mulher-adulta.jpg'
+            }
+            else{
+               img.src = 'imagem/idosa.jpg'
+            }
        }
+       res.style.textAlign = 'center'
        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
    }
 }
