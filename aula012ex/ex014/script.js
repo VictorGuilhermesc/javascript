c5ff28f131dd4a8547e86a1a7e4a327f16c8e096
Victor Guilherme dos Santos Correa <78057195+Victor6345@users.noreply.github.com>
2022-06-26 23:02:45 -0300

    atualização

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,7 +1,20 @@
-function carregar()
-    var mensagem = document.getElementById('msg')
-    var imagem = document.getElementById('imagem')
-    var hora = 8
-    if(hora >= 4 && hora < 12){
-        mensagem.innerHTML = (`São ${hora} da manhã`)
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    var segundos = data.getSeconds()
+    if (hora >= 4 && hora <= 12){
+        msg.innerHTML = `São ${hora}:${minutos}:${segundos}da manhã`
+        img.src = 'imagens/manhã.jpg'
+        document.body.style.background = ''
+    }else if (hora >= 12 && hora < 18){
+        msg.innerHTML = `São ${hora}:${minutos}:${segundos} da tarde`
+        img.src = 'imagens/tarde.jpg'
     }
+    else {
+        msg.innerHTML = `São ${hora}:${minutos}:${segundos} da noite`
+        img.src = 'imagens/noite.jpg'
+    }
+}
